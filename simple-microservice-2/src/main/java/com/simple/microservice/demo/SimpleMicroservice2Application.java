d2d18a0c0b7a0bce47ed11d1f5c9fe92f2658648
ashish8728 <ashish.mca87@gmail.com>
2020-04-23 18:47:46 -0400

    initial simple-mocroservice-2

---
+++ simple-microservice-2/src/main/java/com/simple/microservice/demo/SimpleMicroservice2Application.java
@@ -0,0 +1,13 @@
+package com.simple.microservice.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleMicroservice2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleMicroservice2Application.class, args);
+	}
+
+}
