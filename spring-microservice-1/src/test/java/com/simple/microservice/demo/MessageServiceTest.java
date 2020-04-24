41178aa03e8b00fe680e02403805b3052428e7f7
ashish8728 <ashish.mca87@gmail.com>
2020-04-23 22:18:23 -0400

    added rest template test for rest api test

--- spring-microservice-1/src/test/java/com/simple/microservice/demo/MessageServiceTest.java
+++ spring-microservice-1/src/test/java/com/simple/microservice/demo/MessageServiceTest.java
@@ -1,14 +1,18 @@
 package com.simple.microservice.demo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.web.client.RestTemplate;
 
 @ExtendWith(SpringExtension.class)
 class MessageServiceTest {
@@ -16,9 +20,22 @@ class MessageServiceTest {
 	@InjectMocks
 	MessageService messageService;
 
+	@Mock
+	RestTemplate restTemplate;
+
 	@Test
 	void successMessageTest() {
 		assertEquals("Service is working fine.", messageService.successMessage());
+	}
+
+	@Test
+	void givenSpringMockServerIsUsed_whenGetDataFromOtherServiceIsCalled_shouldReturnMockedObject() throws Exception {
+		String data = null;
+		Mockito.when(restTemplate.getForObject("http://localhost:8080/api/data", String.class))
+				.thenReturn("data returned from other service");
+		data = messageService.getDataFromOtherService();
+		assertNotNull(data);
+		assertEquals("data returned from other service", data);
 	}
 
 }
